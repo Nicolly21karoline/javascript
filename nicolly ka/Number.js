254259ec1a138059f4e0a95e0d1fcd41389e10f9
Nicolly21karoline <nicolly.miyashita@escola.pr.gov.br>
2026-03-12 08:33:04 -0300

    Add files via upload

---
+++ nicolly ka/Number.js
@@ -0,0 +1,12 @@
+const meuNumero = 3
+
+const primeroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeroNumero + segundoNumero
+console.log(operacaoMatematica)
+
+const numeroPontoFlutuante = 3.3
+const PontoFlutuante = .5
+const novaOperacao = primeroNumero / PontoFlutuante
+console.log(novaOperacao)
