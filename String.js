501d2ad021a11a4dd8841eca32373077529a73f9
Nicolly21karoline <nicolly.miyashita@escola.pr.gov.br>
2026-03-19 08:34:47 -0300

    Add files via upload

---
+++ String.js
@@ -0,0 +1,9 @@
+//exemplos de texto
+let nome = "nicolly"
+let sobrenome = "castro"
+let nomecompleto = nome +" "+ sobrenome
+//concatenaçâo das variaveis (+)
+console.log(nomecompleto)
+let senha = "senha1234"
+let stringdenumeros = "123456789"
+console.log(stringdenumeros + 2)
