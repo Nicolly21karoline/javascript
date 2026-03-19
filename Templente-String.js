abdc635a0fe36af89b5bfeba3403ec1838d4d918
Nicolly21karoline <nicolly.miyashita@escola.pr.gov.br>
2026-03-19 08:32:53 -0300

    Add files via upload

---
+++ Templente-String.js
@@ -0,0 +1,6 @@
+const nome = "Nicolly";
+const idade = 2026 - 2010;
+const cidadeNascimento = "curitiba";
+// const apresentacao = "Meu nome é " + nome + "minha idade é" + idade + "e minha cidade de nascimento é" + cidadeNascimento
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e minha cidade de nascimento é ${cidadeNascimento}`
+console.log(apresentacao)
---
+++ Variaveis.js
@@ -0,0 +1,12 @@
+let altura = 5;
+let comprimento =7;
+let area;
+let forma = 'retangulo'
+
+if(forma === 'retangulo'){
+    area = altura * comprimento;
+}else{
+    area = (atura *comprimento) / 2;
+}
+
+console.log(area)
